6c315e5d4ccdb59a5d9226744681788b5476c394
Юлия Кулакова <111242505+Ju909@users.noreply.github.com>
2022-11-03 22:37:47 +0400

    Add files via upload

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,25 @@
+package app.config;
+
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean(name="dog")
+    public Animal getDog() {
+        Animal animal = new Dog();
+        return animal;
+    }
+    @Bean
+    public Timer getTimer() {
+        Timer timer = new Timer();
+        return timer;
+    }
+}
